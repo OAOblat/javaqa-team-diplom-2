45f97c4ad3e8ea5335fd05b0ed1b9f41a7a25fa7
Olga Oblat <oa.oblat@gmail.com>
2023-06-05 22:45:40 +0300

    changed Bank.java & added test

--- src/main/java/ru/netology/javaqadiplom/Bank.java
+++ src/main/java/ru/netology/javaqadiplom/Bank.java
@@ -8,18 +8,22 @@ public class Bank {
      * уменьшиться на эту сумму, а баланс счёта to увеличиться.
      * Если операция прошла неуспешно, балансы обоих счетов никак
      * измениться не должны.
-     * @param from - счёт с которого переводим
-     * @param to - счёт на который переводим
+     *
+     * @param from   - счёт с которого переводим
+     * @param to     - счёт на который переводим
      * @param amount - сумма перевода
      * @return - true если операция прошла успешно, false иначе
      */
     public boolean transfer(Account from, Account to, int amount) {
-        if (amount <= 0) {
+
+        if (amount <= 0 || from.getBalance() < amount) {
             return false;
         }
-        if (from.pay(amount)) {
-            to.add(amount);
+        if (from.getBalance() >= amount) {
+            from.setBalance(from.getBalance() - amount);
+            to.setBalance(to.getBalance() + amount);
+            return true;
         }
-        return true;
+        return false;
     }
 }
