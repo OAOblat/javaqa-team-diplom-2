db5019c41cbaed777678c494dcdd00597be73206
Olga Oblat <oa.oblat@gmail.com>
2023-06-07 01:56:59 +0300

    fix #13

--- src/main/java/ru/netology/javaqadiplom/SavingAccount.java
+++ src/main/java/ru/netology/javaqadiplom/SavingAccount.java
@@ -28,81 +28,89 @@ public class SavingAccount extends Account {
                     "Накопительная ставка не может быть отрицательной, а у вас: " + rate
             );
         }
-            if (minBalance < 0) {
-                throw new IllegalArgumentException(
-                        "Минимальный баланс не может быть отрицательным, а у вас: " + minBalance
-                );
-            }
-            this.balance = initialBalance;
-            this.minBalance = minBalance;
-            this.maxBalance = maxBalance;
-            this.rate = rate;
+        if (minBalance < 0) {
+            throw new IllegalArgumentException(
+                    "Минимальный баланс не может быть отрицательным, а у вас: " + minBalance
+            );
         }
-
-        /**
-         * Операция оплаты с карты на указанную сумму.
-         * В результате успешного вызова этого метода, баланс должен уменьшиться
-         * на сумму покупки. Если же операция может привести к некорректному
-         * состоянию счёта (например, баланс может уйти в минус), то операция должна
-         * завершиться вернув false и ничего не поменяв на счёте.
-         * @param amount - сумма покупки
-         * @return true если операция прошла успешно, false иначе.
-         */
-        @Override
-        public boolean pay ( int amount){
-            if (amount <= 0) {
-                return false;
-            }
-            int balance2 = balance - amount;
-            if (balance2 >= minBalance) {
-                balance = balance2;
-                return true;
-            } else {
-                return false;
-            }
+        if (minBalance > maxBalance) {
+            throw new IllegalArgumentException(
+                    "Минимальный баланс не может быть больше максимального баланса, а у вас: " + minBalance
+            );
         }
+        this.balance = initialBalance;
+        this.minBalance = minBalance;
+        this.maxBalance = maxBalance;
+        this.rate = rate;
+    }
 
-        /**
-         * Операция пополнения карты на указанную сумму.
-         * В результате успешного вызова этого метода, баланс должен увеличиться
-         * на сумму покупки. Если же операция может привести к некорректному
-         * состоянию счёта, то операция должна
-         * завершиться вернув false и ничего не поменяв на счёте.
-         * @param amount - сумма пополнения
-         * @return true если операция прошла успешно, false иначе.
-         * @param amount
-         * @return
-         */
-        @Override
-        public boolean add ( int amount){
-            if (amount <= 0) {
-                return false;
-            }
-            if (balance + amount <= maxBalance) {
-                balance += amount;
-                return true;
-            } else {
-                return false;
-            }
+    /**
+     * Операция оплаты с карты на указанную сумму.
+     * В результате успешного вызова этого метода, баланс должен уменьшиться
+     * на сумму покупки. Если же операция может привести к некорректному
+     * состоянию счёта (например, баланс может уйти в минус), то операция должна
+     * завершиться вернув false и ничего не поменяв на счёте.
+     *
+     * @param amount - сумма покупки
+     * @return true если операция прошла успешно, false иначе.
+     */
+    @Override
+    public boolean pay(int amount) {
+        if (amount <= 0) {
+            return false;
         }
-
-        /**
-         * Операция расчёта процентов на остаток счёта при условии, что
-         * счёт не будет меняться год. Сумма процентов приводится к целому
-         * числу через отбрасывание дробной части (так и работает целочисленное деление).
-         * Пример: если на счёте 200 рублей, то при ставке 15% ответ должен быть 30.
-         * @return
-         */
-        @Override
-        public int yearChange () {
-            return balance / 100 * rate;
-        }
-
-        public int getMinBalance () {
-            return minBalance;
-        }
-
-        public int getMaxBalance () {
-            return maxBalance;
+        int balance2 = balance - amount;
+        if (balance2 >= minBalance) {
+            balance = balance2;
+            return true;
+        } else {
+            return false;
         }
     }
+
+    /**
+     * Операция пополнения карты на указанную сумму.
+     * В результате успешного вызова этого метода, баланс должен увеличиться
+     * на сумму покупки. Если же операция может привести к некорректному
+     * состоянию счёта, то операция должна
+     * завершиться вернув false и ничего не поменяв на счёте.
+     *
+     * @param amount - сумма пополнения
+     * @param amount
+     * @return true если операция прошла успешно, false иначе.
+     * @return
+     */
+    @Override
+    public boolean add(int amount) {
+        if (amount <= 0) {
+            return false;
+        }
+        if (balance + amount <= maxBalance) {
+            balance += amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Операция расчёта процентов на остаток счёта при условии, что
+     * счёт не будет меняться год. Сумма процентов приводится к целому
+     * числу через отбрасывание дробной части (так и работает целочисленное деление).
+     * Пример: если на счёте 200 рублей, то при ставке 15% ответ должен быть 30.
+     *
+     * @return
+     */
+    @Override
+    public int yearChange() {
+        return balance / 100 * rate;
+    }
+
+    public int getMinBalance() {
+        return minBalance;
+    }
+
+    public int getMaxBalance() {
+        return maxBalance;
+    }
+}
