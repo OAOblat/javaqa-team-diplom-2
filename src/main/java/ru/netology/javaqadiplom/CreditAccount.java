c91274f03eb81f198ceac80dff50ec46fe360a42
Nikita Kruglov <bybalesh234@gmail.com>
2023-06-07 19:45:07 +0600

    bag fix. Close issue #4

--- src/main/java/ru/netology/javaqadiplom/CreditAccount.java
+++ src/main/java/ru/netology/javaqadiplom/CreditAccount.java
@@ -52,9 +52,8 @@ public class CreditAccount extends Account {
         if (amount <= 0) {
             return false;
         }
-        balance = balance - amount;
-        if (balance > -creditLimit) {
-            balance = -amount;
+        if ((balance-amount) >= -creditLimit) {
+            balance = balance -amount;
             return true;
         } else {
             return false;
