41cefccf54ea35b7bfd66978024d3138d254359b
Olga Oblat <oa.oblat@gmail.com>
2023-06-08 01:06:34 +0300

    changed BankTest

--- src/test/java/ru/netology/javaqadiplom/BankTest.java
+++ src/test/java/ru/netology/javaqadiplom/BankTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class BankTest {
 
     @Test
-    public void testTransfer() {
+    public void testTransferWhenBalanceEqualsAmount() {
         Account from = new Account(500);
         Account to = new Account(0);
         Bank bank = new Bank();
@@ -14,6 +14,39 @@ public class BankTest {
         Assertions.assertTrue(transferSuccess);
         Assertions.assertEquals(0, from.getBalance());
         Assertions.assertEquals(500, to.getBalance());
+    }
+
+    @Test
+    public void testTransferWhenBalanceMoreThenAmount() {
+        Account from = new Account(600);
+        Account to = new Account(0);
+        Bank bank = new Bank();
+        boolean transferSuccess = bank.transfer(from, to, 500);
+        Assertions.assertTrue(transferSuccess);
+        Assertions.assertEquals(100, from.getBalance());
+        Assertions.assertEquals(500, to.getBalance());
+    }
+
+    @Test
+    public void testTransferWhenAmountNegative() {
+        Account from = new Account(500);
+        Account to = new Account(0);
+        Bank bank = new Bank();
+        boolean transferSuccess = bank.transfer(from, to, -500);
+        Assertions.assertFalse(transferSuccess);
+        Assertions.assertEquals(500, from.getBalance());
+        Assertions.assertEquals(0, to.getBalance());
+    }
+
+    @Test
+    public void testTransferWhenAmount0() {
+        Account from = new Account(500);
+        Account to = new Account(0);
+        Bank bank = new Bank();
+        boolean transferSuccess = bank.transfer(from, to, 0);
+        Assertions.assertFalse(transferSuccess);
+        Assertions.assertEquals(500, from.getBalance());
+        Assertions.assertEquals(0, to.getBalance());
     }
 
     @Test
